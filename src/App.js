51a3da436d86ae1b388c6d5305bdf068bb7a8aca
Reizon Santos <reizonsantos96@gmail.com>
2021-02-12 18:45:13 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Calculator from './main/Calculator'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1>Calculadora</h1>
+          <Calculator />
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
